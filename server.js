6458f06377e0cc4cb9465efd7696b1969aa83112
marc-antoine ourradour <marc-antoineourradour@MacBook-Air-de-marc-antoine.local>
2023-04-13 00:57:01 +0200

    add cors library to enable my local app to call api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const port = process.env.PORT || 6002;
 
@@ -8,7 +9,7 @@ app.get("/", (req, res) => {
 });
 
 const userRouter = require("./routes/verify");
-
+app.use(cors());
 app.use(express.json());
 app.use("/verify", userRouter);
 
